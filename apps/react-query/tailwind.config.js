0e1944dcff96f1ba2e13dac9947e1839dea66903
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-21 09:56:38 -0500

    added global tailwind preset

--- apps/react-query/tailwind.config.js
+++ apps/react-query/tailwind.config.js
@@ -1,7 +1,14 @@
 const { join } = require('path');
 
+// available since Nx v 12.5
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
 module.exports = {
-  purge: [join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}')],
+  presets: [require('../../tailwind-workspace-preset.js')],
+  purge: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},

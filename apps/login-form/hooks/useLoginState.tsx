34c522ba3513c75acad082f160244775bdd23374
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-22 12:01:11 -0500

    simplified

--- apps/login-form/hooks/useLoginState.tsx
+++
@@ -1,6 +0,0 @@
-import { useReducer } from 'react';
-import loginReducer, { initialState } from '../reducers/loginReducer';
-
-export function useLoginState() {
-  return useReducer(loginReducer, initialState);
-}

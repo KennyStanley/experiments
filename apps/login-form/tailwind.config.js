e700aead3efcc5395e77044fd60ca2147237de60
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-22 08:59:15 -0500

    Added TailwindCSS to login-form

---
+++ apps/login-form/tailwind.config.js
@@ -0,0 +1,26 @@
+const { join } = require('path');
+
+// available since Nx v 12.5
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+module.exports = {
+  presets: [require('../../tailwind-workspace-preset.js')],
+  purge: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
+        heading: ['Oswald', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
